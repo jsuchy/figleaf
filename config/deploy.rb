54063a61b379875f9c172f59c7f792ce57fb6dfd
Eric Meyer <ericmeyer@eric-meyers-macbook-pro.local>
2009-08-21 20:37:40 -0500

    big move

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :user, "root"
 set :domain, "#{user}@97.107.138.82"
 set :deploy_to, "/var/apps/fig_leaf"
 set :rails_env, "production"
-set :revision, "fig_leaf"
 # set :web_command, "sudo apache2ctl"
 
   
