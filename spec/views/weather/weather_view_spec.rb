ea21aae8bb64ede2b682b35345f70c216b055472
Eric Meyer <ericmeyer@eric-meyers-macbook-pro.local>
2009-08-23 12:22:49 -0500

    Merge Commit
    stubbed outfit in weather specs

--- spec/views/weather/weather_view_spec.rb
+++ spec/views/weather/weather_view_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "weather view spec" do
   before(:each) do
     @weather = mock(Weather, :null_object => true)
+    Outfit.stub!(:find).and_return(mock(Outfit, :null_object => true))
     assigns[:weather] = @weather
   end
   
