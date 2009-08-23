cd5bd3570d96e3a40c094b593750ab8941d17301
paulwpagel <paul@8thlight.com>
2009-08-23 16:32:29 -0500

    last minute changes to the map

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -85,7 +85,7 @@ class Outfit
   outfit_for :warm, :breezy, :snowy,    {:head => Garment::COWBOY_HAT, :neck => Garment::ASCOT, :body =>  Garment::SNOW_SUIT}
   outfit_for :warm, :breezy, :rainy,    {:head => Garment::RAIN_HAT, :neck => Garment::DOG_TAGS, :body =>  Garment::SHORTS}
               
-  outfit_for :warm, :windy, :sunny,     {:head => Garment::SUNGLASSES, :neck => Garment::ASCOT, :body =>  Garment::SEER_SUCKER}
+  outfit_for :warm, :windy, :sunny,     {:head => Garment::COKE, :neck => Garment::ASCOT, :body =>  Garment::SEER_SUCKER}
   outfit_for :warm, :windy, :cloudy,    {:head => Garment::NRA, :neck => Garment::HANDKERCHIEF, :body =>  Garment::SEER_SUCKER}
   outfit_for :warm, :windy, :snowy,     {:head => Garment::COWBOY_HAT, :neck => Garment::HANDKERCHIEF, :body =>  Garment::SNOW_SUIT}
   outfit_for :warm, :windy, :rainy,     {:head => Garment::DIVE_HELMET, :neck => Garment::DIVE_HELMET_NECK, :body =>  Garment::WET_SUIT}
@@ -97,7 +97,7 @@ class Outfit
   outfit_for :hot, :calm, :rainy,       {:head => Garment::RAIN_HAT, :neck => Garment::DOG_TAGS, :body =>  Garment::LOIN}
                                         
   outfit_for :hot, :breezy, :sunny,     {:head => Garment::SOMBRERO, :neck => Garment::AMULET, :body =>  Garment::SPEEDO}
-  outfit_for :hot, :breezy, :cloudy,    {:head => Garment::COKE, :neck => Garment::SOMBRERO, :body =>  Garment::SPEEDO}
+  outfit_for :hot, :breezy, :cloudy,    {:head => Garment::SOMBRERO, :neck => Garment::DOG_TAGS, :body =>  Garment::SPEEDO}
   outfit_for :hot, :breezy, :snowy,     {:head => Garment::EAR_MUFFS, :neck => Garment::ASCOT, :body =>  Garment::SPEEDO}
   outfit_for :hot, :breezy, :rainy,     {:head => Garment::RAIN_HAT, :neck => Garment::ASCOT, :body =>  Garment::LOIN}
               
